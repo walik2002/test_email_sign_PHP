30ea1a3998459c10873a48cb4e489e7dc3ff5184
Valetin Belousow <bel2002v@yandex.ru>
2023-02-15 20:20:03 +0300

    debugging

--- send.php
+++ send.php
@@ -1,30 +1,39 @@
 <?php
 class Signature {
-  private $name;
-  private $phone1;
-  private $phone2;
-  private $email1;
-  private $email2;
-  private $color;
+  public function __construct(
+    private string $name,
+    private string $phone1,
+    private string $phone2,
+    private string $email1,
+    private string $email2,
+    private string $color
+  ) {}
 
-  public function __construct($name, $phone1, $phone2, $email1, $email2, $color) {
-    $this->name = $name;
-    $this->phone1 = $phone1;
-    $this->phone2 = $phone2;
-    $this->email1 = $email1;
-    $this->email2 = $email2;
-    $this->color = $color;
+  function formatPhoneNumber(string $phone): string {
+    $digits = preg_replace('/[^0-9]/', '', $phone); // Remove non-numeric characters
+    $digits = substr($digits, -9); // Extract last 9 digits
+  
+    $formatted = '+375 ';
+    $formatted .= substr($digits, 0, 2) . ' '; // Add first two digits with a space
+    $formatted .= substr($digits, 2, 3) . '-'; // Add next three digits with a dash
+    $formatted .= substr($digits, 5, 2) . '-'; // Add next two digits with a dash
+    $formatted .= substr($digits, 7); // Add last two digits
+  
+    return $formatted;
   }
 
-  public function generate() {
+  public function generate(): string {
+    $phone1Formatted = $this->formatPhoneNumber($this->phone1);
+    $phone2Formatted = $this->formatPhoneNumber($this->phone2);
+
     $signature = "
       <hr>
-      <p style='color: $this->color;'><strong>Best regards,</strong></p>
+      <p style='color: $this->color;'><strong>С уважением,</strong></p>
       <p style='color: $this->color;'>$this->name</p>
-      <p style='color: $this->color;'>Phone:</p>
+      <p style='color: $this->color;'>Телефон:</p>
       <ul style='color: $this->color;'>
-        <li><a href='tel:$this->phone2'>$this->phone1</a></li>
-        <li><a href='tel:$this->phone2'>$this->phone1</a></li>
+        <li><a href='tel:$phone1Formatted'>$phone1Formatted</a></li>
+        <li><a href='tel:$phone2Formatted'>$phone2Formatted</a></li>
       </ul>
       <p style='color: $this->color;'>Email:</p>
       <ul style='color: $this->color;'>
@@ -37,15 +46,40 @@ class Signature {
   }
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $name = $_POST['name'];
-  $phone1 = $_POST['phone1'];
-  $phone2 = $_POST['phone2'];
-  $email1 = $_POST['email1'];
-  $email2 = $_POST['email2'];
-  $to = $_POST['emailTo'];
-  $message = $_POST['message'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $name = $_POST['name'] ?? '';
+  $phone1 = $_POST['phone1'] ?? '';
+  $phone2 = $_POST['phone2'] ?? '';
+  $email1 = $_POST['email1'] ?? '';
+  $email2 = $_POST['email2'] ?? '';
+  $to = $_POST['emailTo'] ?? '';
+  $message = $_POST['message'] ?? '';
 
+
+  
+  if (!is_string($name)) {
+    throw new InvalidArgumentException('Name must be a string.');
+  }
+  if (!is_string($phone1) || !is_string($phone2)) {
+    throw new InvalidArgumentException('Phone numbers must be strings.');
+  }
+  if (!is_numeric(preg_replace('/[^0-9]/', '', $phone1)) || !is_numeric(preg_replace('/[^0-9]/', '', $phone2))) {
+    throw new InvalidArgumentException('Phone numbers must contain only digits.');
+  }
+  if (!is_string($email1) || !is_string($email2)) {
+    throw new InvalidArgumentException('Email addresses must be strings.');
+  }
+
+  // Validate email addresses
+  if (!filter_var($email1, FILTER_VALIDATE_EMAIL)) {
+    throw new InvalidArgumentException('Invalid email address: ' . $email1);
+  }
+  if (!filter_var($email2, FILTER_VALIDATE_EMAIL)) {
+    throw new InvalidArgumentException('Invalid email address: ' . $email2);
+  }
+  if (!filter_var($to, FILTER_VALIDATE_EMAIL)) {
+    throw new InvalidArgumentException('Invalid email address: ' . $to);
+  }
   // Create first signature object
   $signature1 = new Signature($name, $phone1, $phone2, $email1, $email2, 'red');
 
@@ -54,11 +88,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
   // Compose email message with signatures
   $subject = 'Example Email';
-  $headers = array(
+  $headers = [
     'From: sender@example.com',
     'Content-Type: text/html; charset=UTF-8',
-  );
-  $message = '<p>'.$message.'</p>';
+  ];
+  $message = '<p>' . htmlspecialchars($message) . '</p>'; // Escape HTML characters in message
   $message .= $signature1->generate() . $signature2->generate();
 
   // Send email with signatures
@@ -70,5 +104,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   } else {
     echo 'Error sending email.';
   }
-}
+} 
 ?>
